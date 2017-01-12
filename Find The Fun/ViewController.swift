bf10cf15667c4d8995ff39cf6d0fa49a82495041
Andrea <andrea.rinaldi91@gmail.com>
2017-01-12 17:47:02 +0100

    second commit

--- Find The Fun/ViewController.swift
+++ Find The Fun/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    init() {
+        super.init(nibName: "Homepage", bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Something")
+        print("E' solo una prova")
+        
     }
 
     override func didReceiveMemoryWarning() {
