e17520ce5b2eccc6fca2363617e98f493fbde8e4
Andrea <andrea.rinaldi91@gmail.com>
2017-01-12 17:49:59 +0100

    third commit

--- Find The Fun/ViewController.swift
+++ Find The Fun/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Something")
         print("E' solo una prova")
+        print("aahahah")
         
     }
 
